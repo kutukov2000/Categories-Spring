b7c5f113c28a1bffb5fef9ca250bb81734329941
kutukov2000 <kutukov16.2005@gmail.com>
2024-02-12 09:15:47 +0200

    feat: createCategory ✨

--- src/main/java/com/example/controllers/CategoryController.java
+++ src/main/java/com/example/controllers/CategoryController.java
@@ -1,14 +1,19 @@
 package com.example.controllers;
 
 import lombok.AllArgsConstructor;
+
+import com.example.entities.CategoryCreateModel;
 import com.example.entities.CategoryEntity;
 import com.example.repositories.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -21,5 +26,15 @@ public class CategoryController {
     public ResponseEntity<List<CategoryEntity>> index() {
         List<CategoryEntity> list = categoryRepository.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping
+    CategoryEntity createCategory(@RequestBody CategoryCreateModel newCategory){
+        CategoryEntity category=new CategoryEntity();
+        category.setName(newCategory.getName());
+        category.setDescription(newCategory.getDescription());
+        category.setImage(newCategory.getImage());
+        category.setCreationTime(LocalDateTime.now());
+        return categoryRepository.save(category);
     }
 }
---
+++ src/main/java/com/example/entities/CategoryCreateModel.java
@@ -0,0 +1,10 @@
+package com.example.entities;
+
+import lombok.Data;
+
+@Data
+public class CategoryCreateModel {
+    private String name;
+    private String image;
+    private String description;
+}
